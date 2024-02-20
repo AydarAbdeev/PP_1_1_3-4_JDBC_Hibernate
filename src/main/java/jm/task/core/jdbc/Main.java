6ad099817470948cf19097d2cdd768e645b64272
AydarAbdeev <Abdey21140@gmail.com>
2024-02-21 01:10:25 +0500

    main fix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
 public class Main {
@@ -14,5 +15,15 @@ public class Main {
         System.out.println(userDao.getAllUsers());
         userDao.cleanUsersTable();
         userDao.dropUsersTable();
+
+        UserDao userDaoHibernate = new UserDaoHibernateImpl();
+        userDaoHibernate.createUsersTable();
+        userDaoHibernate.saveUser("Tony", "Kroos", (byte) 34);
+        userDaoHibernate.saveUser("Luka", "Modric", (byte) 38);
+        userDaoHibernate.saveUser("Jude", "Bellingham", (byte) 20);
+        userDaoHibernate.saveUser("Dani", "Carvajal", (byte) 32);
+        System.out.println(userDaoHibernate.getAllUsers());
+        userDaoHibernate.cleanUsersTable();
+        userDaoHibernate.dropUsersTable();
     }
 }
